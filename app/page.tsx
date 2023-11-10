bc001ea91834796e5b54527db5b07378692b2343
Quyen Hoang <qduhoang@gmail.com>
2023-11-09 16:17:59 -0800

    simple counter done

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+'use client';
+import { useState } from 'react';
+
 export default function Home() {
-  return <div>test</div>;
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  return (
+    <div className="h-[100vh] flex flex-col justify-center items-center">
+      <p>{count}</p>
+      <button
+        className="p-2 border-2 rounded-lg border-gray-700"
+        onClick={incrementCount}
+      >
+        Increment
+      </button>
+    </div>
+  );
 }
