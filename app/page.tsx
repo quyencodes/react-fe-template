cae5b17dcbbf2ad0aaf375dead5198f1f4217f51
Quyen Hoang <qduhoang@gmail.com>
2023-11-09 16:22:30 -0800

    resetting repo

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,3 @@
-'use client';
-import { useState } from 'react';
-
 export default function Home() {
-  const [count, setCount] = useState(0);
-
-  const incrementCount = () => {
-    setCount((prev) => prev + 1);
-  };
-
-  return (
-    <div className="h-[100vh] flex flex-col justify-center items-center">
-      <p>{count}</p>
-      <button
-        className="p-2 border-2 rounded-lg border-gray-700"
-        onClick={incrementCount}
-      >
-        Increment
-      </button>
-    </div>
-  );
+  return <div></div>;
 }
